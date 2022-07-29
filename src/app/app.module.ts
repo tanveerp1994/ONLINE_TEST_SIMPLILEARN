6028038d3bfdc70fe1391e1b2e2d9536ada17b36
tanveerp1994@gmail.com <tanveerp1994@gmail.com>
2022-07-29 16:40:28 +0530

    PHASE-04_SIMPLILEARN

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import { QuizComponent } from './quiz/quiz.component'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    QuizComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/services/quiz.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class QuizService {
+
+  constructor(private http: HttpClient) { }
+
+  get(url: string) {
+    return this.http.get(url);
+  }
+
+  getAll() {
+    return [
+      { id: 'http://localhost:3000/profile', name: 'Java' },
+      { id: 'data/aspnet.json', name: 'Asp.Net' },
+      { id: 'data/csharp.json', name: 'C Sharp' },
+      { id: 'data/designPatterns.json', name: 'Design Patterns' }
+    ];
+  }
+
+}
